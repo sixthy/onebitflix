3c89edd07a7bcef7da463c5f143dd9964bc776cf
Gabriel <gabrielferreirasantos723@gmail.com>
2022-10-04 16:48:58 -0300

    adicionado a comunicação com o adminJS

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
 import express from "express"
+import { adminJs, adminJsRouter } from "./adminjs"
 import { sequelize } from "./database";
 
+
 const App = express();
+
+App.use(express.static('public'));
+
+App.use(adminJs.options.rootPath, adminJsRouter);
+
 
 const PORT = process.env.PORT || 3000;
 
