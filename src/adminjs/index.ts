e3aeaebcd9b8d2731008de9158c1d34df72d2e2b
Gabriel <gabrielferreirasantos723@gmail.com>
2022-10-05 10:05:05 -0300

    recursos usados dentro do painel

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -2,15 +2,17 @@ import AdminJS from "adminjs";
 import AdminJSExpress from "@adminjs/express";
 import AdminJSSequelize from "@adminjs/sequelize";
 import { sequelize } from "../database";
+import { adminJsResources } from "./resources";
 
 AdminJS.registerAdapter(AdminJSSequelize);
 
 export const adminJs = new AdminJS({
     databases: [sequelize],
     rootPath: "/admin",
+    resources: adminJsResources,
     branding: {
         companyName: 'OneBitFlix',
-        logo: '/onebitflix.svg',
+        logo: '/logoOnebitflix.svg',
         theme: {
             colors: {
                 primary100: '#ff0043',
